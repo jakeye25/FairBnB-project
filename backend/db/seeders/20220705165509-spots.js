dc104739e91d5d503b87f5bf2f4f62da2126c86f
Jake Ye <jayyeucla@gmail.com>
2022-07-05 21:30:37 -0700

    working on spot seed

--- backend/db/seeders/20220705165509-spots.js
+++ backend/db/seeders/20220705165509-spots.js
@@ -2,15 +2,29 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('Spots', [
+      {
+        email: 'user1@user.io',
+        username: 'FakeUser1',
+        firstName: "firstName1",
+        lastName: "Lastname1",
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user2@user.io',
+        username: 'FakeUser2',
+        firstName: "firstName2",
+        lastName: "Lastname2",
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'user3@user.io',
+        username: 'FakeUser3',
+        firstName: "firstName3",
+        lastName: "Lastname3",
+        hashedPassword: bcrypt.hashSync('password3')
+      }
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
