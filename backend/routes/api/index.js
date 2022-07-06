d82cdafba9f26058242d749c40c0c34d41584284
Jake Ye <jayyeucla@gmail.com>
2022-07-06 16:22:55 -0700

    get all spots updated

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,13 +3,14 @@ const router = require('express').Router();
 const { restoreUser } = require('../../utils/auth.js');
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const spotsRouter = require('./spots.js');
 router.use(restoreUser);
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-// const spotsRouter = require('./spots');
-// router.use('/spots', spotsRouter);
+
+router.use('/spots', spotsRouter);
 
 // backend/routes/api/index.js
 // ...
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,11 +1,22 @@
 const express = require('express');
-
+const { restoreUser } = require('../../utils/auth');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { Spot, User, Review, Booking, Image } = require('../../db/models');
-
+// const user = require('../db/models/user');
 // ...
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 // ...
 
 const router = express.Router();
+
+
+//get all spots
+router.get('/', async (req, res) => {
+    const spots = await Spot.findAll()
+    res.json(spots)
+  })
+
+
+
+module.exports = router;
