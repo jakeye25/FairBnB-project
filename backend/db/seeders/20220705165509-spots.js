4a2b16fef91c5978a05bbac802ea36f8d7ae56e9
Jake Ye <jayyeucla@gmail.com>
2022-07-05 23:31:21 -0700

    spot seed data added

--- backend/db/seeders/20220705165509-spots.js
+++ backend/db/seeders/20220705165509-spots.js
@@ -1,38 +1,53 @@
 'use strict';
 
+const spots = [
+  {
+    "ownerId": 1,
+    "address": "123 Disney Lane",
+    "city": "San Francisco",
+    "state": "California",
+    "country": "United States of America",
+    "lat": 37.7645358,
+    "lng": -122.4730327,
+    "name": "App Academy",
+    "description": "spot 1",
+    "price": 123,
+    "previewImage": "image1"
+  },
+  {
+    "ownerId": 2,
+    "address": "456 Disney Lane",
+    "city": "San Francisco",
+    "state": "California",
+    "country": "United States of America",
+    "lat": 40.7645358,
+    "lng": -120.4730327,
+    "name": "App Academy",
+    "description": "second spot",
+    "price": 456,
+    "previewImage": "image2"
+  },
+  {
+    "ownerId": 3,
+    "address": "3 Disney Lane",
+    "city": "San Francisco",
+    "state": "California",
+    "country": "United States of America",
+    "lat": 41.7645358,
+    "lng": -123.4730327,
+    "name": "App Academy",
+    "description": "third spot",
+    "price": 789,
+    "previewImage": "image3"
+  }
+];
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Spots', [
-      {
-        email: 'user1@user.io',
-        username: 'FakeUser1',
-        firstName: "firstName1",
-        lastName: "Lastname1",
-        hashedPassword: bcrypt.hashSync('password')
-      },
-      {
-        email: 'user2@user.io',
-        username: 'FakeUser2',
-        firstName: "firstName2",
-        lastName: "Lastname2",
-        hashedPassword: bcrypt.hashSync('password2')
-      },
-      {
-        email: 'user3@user.io',
-        username: 'FakeUser3',
-        firstName: "firstName3",
-        lastName: "Lastname3",
-        hashedPassword: bcrypt.hashSync('password3')
-      }
-    ], {});
+    return queryInterface.bulkInsert('Spots', spots, {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('Spots', {[Op.or] : spots}, {});
   }
 };
