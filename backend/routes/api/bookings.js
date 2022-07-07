06f57131e620c3e38a8836158c834dfb781eee5f
Jake Ye <jayyeucla@gmail.com>
2022-07-06 23:39:17 -0700

    working on spot

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Spot, User, Review, Booking, Image } = require('../../db/models');
+const { Spot, User, Review, Booking, Image, sequelize } = require('../../db/models');
 
 // ...
 const { check } = require('express-validator');
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { restoreUser } = require('../../utils/auth');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Spot, User, Review, Booking, Image } = require('../../db/models');
+const { Spot, User, Review, Booking, Image, sequelize } = require('../../db/models');
 // const user = require('../db/models/user');
 // ...
 const { check } = require('express-validator');
@@ -17,6 +17,34 @@ router.get('/', async (req, res) => {
     res.json(spots)
   })
 
+//get spot by soptId
+router.get('/:id', async (req, res, next) => {
+    const spots = await Spot.findAll({
+        where:{ id: req.params.id},
+        attributes:{
+            include:[
+                [
+                    sequelize.fn("COUNT", sequelize.col("Reviews.review")), "numReviews"
+                ],
+                [
+                    sequelize.fn("AVG", sequelize.col("Reviews.stars")), "avgStarRating"
+                ]
+            ]
+        },
+        include: [{
+            model: Review,
+            as: 'reviews',
+            attributes: []
+          }],
+});
 
-
+    if(!spots) {
+        const err = Error("Spot couldn't be found");
+        err.status = 404;
+        err.title = "Spot couldn't be found";
+        err.message = "Spot couldn't be found";
+        return next(err);
+    }
+    res.json(spots)
+  })
 module.exports = router;
