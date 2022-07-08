3430351ae02989becfbbbe1221f0f687cc5db1d2
Jake Ye <jayyeucla@gmail.com>
2022-07-08 14:40:45 -0700

    delete booking done

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -7,6 +7,7 @@ const { Op } = require('sequelize');
 // ...
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const booking = require('../../db/models/booking');
 // ...
 
 const router = express.Router();
@@ -16,5 +17,35 @@ router.get('/', async (req, res) => {
     const bookings = await Booking.findAll()
     res.json(bookings)
   })
+
+//delete booking
+router.delete(
+    '/:bookingId', restoreUser, requireAuth,
+    async (req, res, next) => {
+
+    let bkId = req.params.bookingId;
+
+    const deleteBooking = await Booking.findByPk(bkId);
+
+    let todayDate = new Date().toISOString().slice(0, 10)
+        // return res.json(deleteBooking.startDate)
+    if(!deleteBooking) {
+      return res.status(404).json({message: "Booking couldn't be found", statusCode: 404})}
+// console.log(deleteBooking)
+// res.send('delete booking')
+    if(deleteBooking.startDate < todayDate) {
+       return res.status(400).json({message: "Bookings that have been started can't be deleted",
+  statusCode: 400})}
+
+//     else{
+    // await deleteBooking.destroy()
+    await Booking.destroy({where : {id: req.params.bookingId}})
+    // deleteBooking.save()
+         res.status(200)
+         res.json({message: "Successfully deleted", statusCode: 200})
+        // res.send('deleted')
+//     }
+    }
+    )
 
 module.exports = router;
