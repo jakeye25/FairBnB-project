b8635bb34fc7da6e5043de5ae48373a3a4fd9a9c
Jake Ye <jayyeucla@gmail.com>
2022-07-08 15:59:51 -0700

    edit booking done

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -3,6 +3,7 @@ const { restoreUser } = require('../../utils/auth');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { Spot, User, Review, Booking, Image, sequelize } = require('../../db/models');
 const user = require('../../db/models/user');
+const spot = require('../../db/models/spot');
 const { Op } = require('sequelize');
 // ...
 const { check } = require('express-validator');
@@ -17,6 +18,45 @@ router.get('/', async (req, res) => {
     const bookings = await Booking.findAll()
     res.json(bookings)
   })
+
+//edit a booking
+router.put(
+    '/:bookingId', restoreUser,
+    async (req, res, next) => {
+        let {startDate, endDate} = req.body;
+
+        let bookingId = req.params.bookingId;
+    const editBooking = await Booking.findByPk(bookingId);
+
+    if(!editBooking) {
+        res.status(404).json({message: "Booking couldn't be found",
+    statusCode: 404})}
+
+    let todayDate = new Date().toISOString().slice(0, 10)
+
+    if(startDate > endDate || startDate<todayDate || endDate< todayDate) {
+        res.status(403).json({
+            message: "Sorry, this spot is already booked for the specified dates",
+            statusCode: 403,
+            "errors": {
+              "startDate": "Start date conflicts with an existing booking",
+              "endDate": "End date conflicts with an existing booking"}
+          })
+    }
+
+
+  if(editBooking.endDate < todayDate) {
+     res.status(400).json({message: "Past bookings can't be modified",
+statusCode: 400})}
+
+
+    editBooking.startDate = startDate
+    editBooking.endDate = endDate
+
+    await editBooking.save()
+    res.status(200).json(editBooking)
+    })
+
 
 //delete booking
 router.delete(
