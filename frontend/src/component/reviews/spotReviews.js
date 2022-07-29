0761954de0053fbf2d5005a1132412ef1d970d65
Jake Ye <jayyeucla@gmail.com>
2022-07-29 12:08:08 -0700

    edit review

--- frontend/src/component/reviews/spotReviews.js
+++ frontend/src/component/reviews/spotReviews.js
@@ -12,31 +12,31 @@ const SpotReviews = () => {
 
     useEffect(() => {
         dispatch(getSpotReviews(id));
-      }, [dispatch, id]);
+      }, [dispatch]);
 
       if (!reviews.length) {
         return null;
       }
 
       return (
-        <h1>testing</h1>
-        // <>
+        // <h1>testing</h1>
+        <>
+            <h1>testing</h1>
+            {reviews.map((review) => (
+            <div key={review.id}>
+              {/* <NavLink key={review.id} to={`/reviews/${review.id}`}> */}
 
-        //     {reviews.map((review) => (
-        //     <span key={review.id}>
-        //       {/* <NavLink key={review.id} to={`/reviews/${review.id}`}> */}
+                {/* <div className="centered">spotId: {review.spotId}</div> */}
+                <div className="centered">Review: {review.review}</div>
+                <div className="centered">Stars Rating: {review.stars}</div>
+              {/* </NavLink> */}
 
-        //         {/* <div className="centered">spotId: {review.spotId}</div> */}
-        //         <div className="centered">Review: {review.review}</div>
-        //         <div className="centered">Stars Rating: {review.stars}</div>
-        //       {/* </NavLink> */}
-
-        //         {/* <button onClick={() => dispatch(deleteReview(review.id))}>
-        //             Delete
-        //          </button> */}
-        //     </span>
-        // ))}
-        // </>
+                {/* <button onClick={() => dispatch(deleteReview(review.id))}>
+                    Delete
+                 </button> */}
+            </div>
+        ))}
+        </>
     );
 }
 
