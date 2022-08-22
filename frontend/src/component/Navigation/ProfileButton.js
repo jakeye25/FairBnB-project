702e35e7b50879db5bf8520903191f2fb244b458
Jake Ye <jayyeucla@gmail.com>
2022-08-21 20:49:42 -0700

    css updating

--- frontend/src/component/Navigation/ProfileButton.js
+++ frontend/src/component/Navigation/ProfileButton.js
@@ -34,6 +34,7 @@ function ProfileButton({ user }) {
     <>
       <button onClick={openMenu}>
         <i className="fas fa-user-circle" />
+        {/* <i className="fas fa-duotone fa-square-user" /> */}
       </button>
       {showMenu && (
         <ul className="profile-dropdown">
