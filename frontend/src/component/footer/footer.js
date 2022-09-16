be61fda6a155c51f19a473e7866a1eeab977751e
Jake Ye <jayyeucla@gmail.com>
2022-09-16 16:58:48 -0700

    working on footer

--- frontend/src/component/footer/footer.js
+++ frontend/src/component/footer/footer.js
@@ -9,13 +9,15 @@ export default function Footer (){
     return (
         <>
             <div>
-
+                <p>
+                    
+                </p>
             </div>
             <NavLink>
-
+                <img src='https://icons8.com/icon/qNUNvR9aEWql/linkedin-circled' alt='linkedin logo'></img>
             </NavLink>
             <NavLink>
-                
+                <img src='https://icons8.com/icon/3tC9EQumUAuq/github' alt='github logo'></img>
             </NavLink>
         </>
     )
