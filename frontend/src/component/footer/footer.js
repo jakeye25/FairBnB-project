170d8857ed62d3c1cb827f296bb1ec3d1bf08f84
Jake Ye <jayyeucla@gmail.com>
2022-09-16 16:51:28 -0700

    add footer

---
+++ frontend/src/component/footer/footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Footer.css';
+
+
+export default function Footer (){
+
+
+    return (
+        <>
+            <div>
+
+            </div>
+            <NavLink>
+
+            </NavLink>
+            <NavLink>
+                
+            </NavLink>
+        </>
+    )
+}
